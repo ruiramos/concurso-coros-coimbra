73cbbb7f34b1896572d742c7e64490f66eccd2c7
Rui Ramos <ruiramos@gmail.com>
2024-02-25 21:08:20 +0000

    Update homepage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,20 +28,38 @@ const IndexPage = ({ data }) => (
       <p>
         O concurso de coros visa não só homenagear José António Carlos Seixas,
         compositor de destaque do barroco português, nado em Coimbra, em 1704,
-        mas, sobretudo, dinamizar a música coral tendo como palco a cidade de
-        Coimbra, cidade universitária de cariz e de matriz eminentemente
-        culturais, detentora de assinalável tradição neste campo musical.
+        mas também dinamizar música coral tendo como palco a cidade de Coimbra,
+        cidade universitária de cariz e de matriz eminentemente culturais,
+        detentora de assinalável tradição neste campo musical.
       </p>
       <p>
         Encontram-se disponiveis para download o{" "}
         <a target="_blank" href="/2024/Regulamento_III_Concurso.pdf">
           Regulamento do III Concurso
         </a>
-        , bem como a{" "}
+        , bem como a obra obrigatória -{" "}
         <a target="_blank" href="/2024/PartituraIIIConcursoCoros.pdf">
-          obra obrigatória - Balada para Coimbra, de José Firmino
+          Balada para Coimbra, de José Firmino
         </a>
         .
+      </p>
+      <p>
+        No &quot;III Concurso de Coros – Coimbra 2024&#39;&#39; pretende-se
+        ainda prestar a devida homenagem ao maestro José Firmino, através da
+        escolha da peça obrigatória - “Balada para Coimbra”. José Firmino,
+        maestro recentemente falecido, foi fundador de dois grupos corais de
+        Coimbra - o Choral Poliphonico e o Coro dos Pequenos Cantores, tendo
+        sido ainda autor de vasta obra coral e sinfónica.
+      </p>
+      <p>
+        O &quot;III Concurso de Coros - Coimbra&quot; integrará duas fases: a
+        primeira será uma fase de pré-seleção, com todos os coros amadores
+        inscritos; a segunda, a realizar no dia 09 de junho de 2024, terá a
+        participação de oito coros selecionados . O <Link to="/juri">Júri</Link>{" "}
+        é constituído por prestigiados maestros de renome nacional e
+        internacional. O evento terminará com um Concerto de Gala, à noite, com
+        a participação dos três primeiros coros classificados e do anfitrião,
+        Coro Carlos Seixas.
       </p>
     </Section>
   </Layout>
